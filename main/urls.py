e57cdd5290115f176357237bc7f0d895c73b7a26
Kr4v3n <fafachen37@gmail.com>
2021-06-21 13:14:29 +0200

    Add about page

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 
 urlpatterns = [
 
-    url('', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
+    url(r'^about/$', views.about, name='about'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -3,5 +3,8 @@ from .models import Main
 
 
 def home(request):
+    return render(request, 'front/home.html')
 
-    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'front/about.html')
