abeafbc60e888b71bd07fdf78377347c4ffb48c2
Kr4v3n <fafachen37@gmail.com>
2021-06-22 14:33:41 +0200

    Add article

--- main/views.py
+++ main/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from .models import Main
+from articles.models import Articles
 
 
 def home(request):
     site = Main.objects.get(pk=4)
+    articles = Articles.objects.all()
 
-    return render(request, 'front/home.html', {'site': site
-                                               })
+    return render(request, 'front/home.html',
+                  {'site': site,
+                   'articles': articles
+                  })
 
 
 def about(request):
-
     site = Main.objects.get(pk=4)
 
     return render(request, 'front/about.html', {'site': site
