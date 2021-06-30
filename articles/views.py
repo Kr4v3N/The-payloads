5113bb503c3f7656ef21e789e8da96cd5d0246ae
Kr4v3n <fafachen37@gmail.com>
2021-06-30 10:00:36 +0200

    Validations in forms add article

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from .models import Articles
 from main.models import Main
+
+
+def articles_list(request):
+    articles = Articles.objects.all()
+    return render(request, 'back/articles_list.html', {
+        'articles': articles
+    })
 
 
 def article_detail(request, word):
@@ -13,22 +21,20 @@ def article_detail(request, word):
                    })
 
 
-def articles_list(request):
-    articles = Articles.objects.all()
-    return render(request, 'back/articles_list.html', {
-        'articles': articles
-    })
-
-
 def articles_add(request):
 
     if request.method == 'POST':
+
         articletitle = request.POST.get('articletitle')
         articlecategory = request.POST.get('articlecategory')
         articletxtshort = request.POST.get('articletxtshort')
         articletxt = request.POST.get('articletxt')
 
-        print(articletxt, articletxtshort,
-              articlecategory, articletitle)
+        if articletitle == "" \
+                or articletxtshort == "" \
+                or articletxt == "" \
+                or articlecategory == "":
+            error = "Tous les champs sont requis"
+            return render(request, 'back/messages.html', {'error': error})
 
     return render(request, 'back/articles_add.html')
