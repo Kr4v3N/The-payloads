496f2f2a509116fec7b8412a45842ebe053fedc0
Kr4v3n <fafachen37@gmail.com>
2021-09-11 00:08:20 +0200

    Lock Admin page

--- main/views.py
+++ main/views.py
@@ -3,19 +3,23 @@ from django.shortcuts import render, get_object_or_404, redirect
 from category.models import Category
 from .models import Main
 from articles.models import Articles
-
+from subcategory.models import Subcategory
 
 def home(request):
     site = Main.objects.get(pk=4)
     articles = Articles.objects.all().order_by('-pk')
     cat = Category.objects.all()
     lastarticles = Articles.objects.order_by('-pk')[:4]
+    lastarticles2 = Articles.objects.order_by('-pk')[:4]
+    subcat = Subcategory.objects.all()
 
     return render(request, 'front/home.html', {
         'site': site,
         'articles': articles,
         'category': cat,
         'lastarticles': lastarticles,
+        'lastarticles2': lastarticles2,
+        'subcat': subcat
     })
 
 
