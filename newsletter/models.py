99ac879c46fffed771c5369c03949b31e2b2f140
Kr4v3n <fafachen37@gmail.com>
2021-09-30 16:33:37 +0200

    Added send email without settings

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 
 
-# Create your models here.
-
 class Newsletter(models.Model):
-    email = models.EmailField(max_length=100, blank=True, unique=True)
+    txt = models.EmailField(max_length=100, blank=True, unique=True)
     status = models.IntegerField()
     date = models.CharField(max_length=12)
     time = models.CharField(max_length=12, default="00H00")
     phone_number = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
-        return self.email
+        return self.txt
 
     class Meta:
         verbose_name = 'Newsletter'
